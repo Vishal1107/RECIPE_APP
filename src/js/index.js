a09acbc7c5f6ad344b9d027999459a1056427242
Vishal_Bait <vishalbait01@gmail.com>
2020-06-29 22:50:07 +0530

    Add Search model and controller code in index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,23 @@
-import x from "./test";
-const y = 23;
+import Search from "./models/Search";
 
-console.log(`This is export ${x} and the value is ${y}`);
+/**  Global Object
+ * - Search Object
+ * - Current Recipe Object
+ * - Shopping List Object
+ * - Like Recipe
+ */
+const state = {};
+
+const controlSearch = async () => {
+  let query = "pizza";
+  if (query) {
+    state.search = new Search(query);
+    await state.search.getResult();
+    console.log(state.search.result);
+  }
+};
+
+document.getElementById("searchForm").addEventListener("submit", (e) => {
+  e.preventDefault();
+  controlSearch();
+});
---
+++ src/js/models/Search.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+class Search {
+  constructor(query) {
+    this.query = query;
+  }
+
+  async getResult() {
+    try {
+      let res = await axios.get(
+        `https://forkify-api.herokuapp.com/api/search?&q=${this.query}`
+      );
+      this.result = res.data.recipes;
+    } catch (error) {
+      alert(error);
+    }
+  }
+}
+
+export default Search;
